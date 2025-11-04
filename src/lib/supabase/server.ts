0605ebee9d9f18319857f4ab674ea41876b29735
FelipePSantiago <santiago.physics@gmail.com>
2025-11-04 17:35:55 -0300

    Correção Deploy

--- src/lib/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-export async function createClient() {
-  const supabase = createServerComponentClient({ cookies })
-  return supabase
-}
